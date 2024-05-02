52223ed6fd626992e7c3e3b6c7eb0d6480fd85d6
jayp5545 <110543100+jayp5545@users.noreply.github.com>
2024-05-02 19:02:04 -0300

    Added 3-promise-all

--- Assignments/Week2/01-async-js/hard (promises)/3-promise-all.js
+++ Assignments/Week2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,40 @@
  */
 
 function wait1(t) {
-
+    return new Promise((res)=>{
+        setTimeout(()=>{
+            res();
+        },t*1000)
+    })
 }
 
 function wait2(t) {
-
+    return new Promise((res)=>{
+        setTimeout(()=>{
+            res();
+        },t*1000)
+    })
 }
 
 function wait3(t) {
-
+    return new Promise((res)=>{
+        setTimeout(()=>{
+            res();
+        },t*1000)
+    })
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const cTime = Date.now();
+    return Promise.all([
+        wait1(t1),
+        wait2(t2),
+        wait3(t3)])
+        .then(()=>{
+            return Date.now() - cTime;
+        });
 }
-
+calculateTime(1,2,3).then((data)=>{
+    console.log(data);
+});
 module.exports = calculateTime;
