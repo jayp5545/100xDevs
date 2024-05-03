80780a0ab8f28d6c16e97fff14cfd8a875acd53d
jayp5545 <110543100+jayp5545@users.noreply.github.com>
2024-05-03 17:40:23 -0300

    Added jwt files

--- Assignments/Week3/01-middlewares/03-errorcount.js
+++ Assignments/Week3/01-middlewares/03-errorcount.js
@@ -10,6 +10,10 @@ let errorCount = 0;
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
+app.use((err,req,res,next)=>{
+  errorCount++;
+  console.log(err.stack)
+})
 app.get('/user', function(req, res) {
   throw new Error("User not found");
   res.status(200).json({ name: 'john' });
